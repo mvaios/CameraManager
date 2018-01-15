d4801176fb1182b2eed137d6870c2165e9dc430a
Vahram Mirzoyan <vahram@tickaroo.com>
2018-01-15 10:37:54 +0100

    Set content mode to fit

--- camera/CameraManager.swift
+++ camera/CameraManager.swift
@@ -997,7 +997,7 @@ open class CameraManager: NSObject, AVCaptureFileOutputRecordingDelegate, UIGest
     fileprivate func _setupPreviewLayer() {
         if let validCaptureSession = captureSession {
             previewLayer = AVCaptureVideoPreviewLayer(session: validCaptureSession)
-            previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspect
         }
     }
 
